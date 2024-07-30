9ec1a8996e8d8833da052b83915520f1a64eb49f
Hossain Delwar <hmdelwar718@gmail.com>
2024-07-30 11:21:53 -0400

    first

--- app/page.js
+++ app/page.js
@@ -1,5 +1,11 @@
+import { Box, Container, Stack, Typography } from "@mui/material";
+import car from './img/Shake-Shack-Logo.png'
 
+const item = ['tomato', 'carrot', 'potato','garbanzo beans']
 export default function Home() {
-  return (
-  );
+  return(
+    <div>
+         <img src="Shake-Shack-Logo.png" alt="this is car image" />        
+    </div>
+)
 }
